47ab1471c6019ad3c4b486c58eba9c49d7db1317
vinsensiuschristo <hallavinsen@gmail.com>
2021-10-07 10:33:16 +0700

    On Click BottomNavigation Done

--- app/src/main/java/com/example/alp/Activity/DetailProjectActivity.java
+++ app/src/main/java/com/example/alp/Activity/DetailProjectActivity.java
@@ -1,16 +1,50 @@
 package com.example.alp.Activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.example.alp.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class DetailProjectActivity extends AppCompatActivity {
+public class DetailProjectActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_project);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+//        bottomNavigationView.getSelectedItemId(R.id.map);
+
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.map:
+                Toast.makeText(getApplicationContext(), "map ", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.unfinished:
+                Toast.makeText(getApplicationContext(), "unfinished ", Toast.LENGTH_SHORT).show();
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new UnfinishedFragment()).commit();
+                return true;
+            case R.id.notUpload:
+                Toast.makeText(getApplicationContext(), "notUpload ", Toast.LENGTH_SHORT).show();
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new NotuploadedFragment()).commit();
+                return true;
+            case R.id.upload:
+                Toast.makeText(getApplicationContext(), "upload ", Toast.LENGTH_SHORT).show();
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new UploadedFragment()).commit();
+                return true;
+        }
+        return false;
     }
 }
